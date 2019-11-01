087a8ae9939cc9eb13c8aaeb313fd9aaddce9909
Aaron Dunford <mraaronnom@live.co.uk>
2019-11-01 13:27:07 +0000

    v0.4.1, script restructure

--- lua/Tabber.lua
+++ lua/Tabber.lua
@@ -1,45 +1,48 @@
-currentTab = nil
-mainTab = nil
-userTabs = nil
-switchTab = false
-devMode = false
-dClose = false
-recentWindow = nil
+-- global variables
 
-function onFirstLayout()
-	registerMenuItem("New Tab", "insert", 5)
+currentTab 		= nil;
+userTabs 		= nil;
+devMode 		= false;
+
+-- Data for a temp tab
+-- TODO: Make use of this tempTab skeleton
+
+tempTab = {
+	["init"] = {},
+	["data"] = {},
+	["text"] = ""
+};
+
+-- Init the campaign registry and register menu items.
+-- Load tabs if the campaign registry information already exists
+
+function onInit()
+	registerMenuItem("New Tab", "insert", 5);
 
 	if CampaignRegistry["Tabber"] == nil then
-		CampaignRegistry["Tabber"] = {}
+		CampaignRegistry["Tabber"] = {};
 	end
 
 	if CampaignRegistry["Tabber"][User.getUsername()] == nil then
-		devMessage( "New User Init" )
-
-		CampaignRegistry["Tabber"][User.getUsername()] = {}
-		userTabs = CampaignRegistry["Tabber"][User.getUsername()]
-		createMain()
+		CampaignRegistry["Tabber"][User.getUsername()] = {};
+		userTabs = CampaignRegistry["Tabber"][User.getUsername()];
 	else
-		devMessage( "Registered User Init" )
-
-		userTabs = CampaignRegistry["Tabber"][User.getUsername()]
-		loadTabs()
-		resetAnchors()
+		userTabs = CampaignRegistry["Tabber"][User.getUsername()];
+		loadTabs();
 	end
 
-	changeTab( mainTab )
-	Interface.onWindowOpened = saveWindow
-	Interface.onWindowClosed = removeWindow
-	Interface.onDesktopClose = desktopClose
 end
+
+-- Menu options
 
 function onMenuSelection( selection )
+	-- Add Tab
 	if selection == 5 then
-		addTab()
+		addTab();
 	end
 end
 
--- Custom functions
+-- Send toggled message to console
 
 function devMessage( message )
 	if devMode then
@@ -47,229 +50,104 @@ function devMessage( message )
 	end
 end
 
-function createMain()
-	mainTab = createControl( "tabbertab", "TAB1" )
-	mainTab.setValue("Home")
-	storeTab( mainTab )
-	currentTab = mainTab
-end
-
-function addTab()
-	devMessage( "Adding tab" )
-
-	tabs = getControls()
-	num = tonumber( string.sub(tabs[#tabs].getName(), 4) ) + 1
-
-	newTab = createControl( "tabbertab", "TAB" .. num )
-	newTab.setValue( "New Tab " .. num )
-	newTab.canDelete()
-
-	closeAllWindows( )	
-	storeTab( newTab )
-	tabGainFocus( newTab )
-	resetAnchors()
-	devMessage( "Tab added:" .. num )
-end
+-- Load in order of name
+-- TODO: add custom ordering a drag & drop ordering
 
 function loadTabs()
-	devMessage( "Loading tabs" )
+	local num = nil;
+	local numOrder = {};
 
-	local num = nil
-	local numOrder = {}
-
-	for name,value in pairs(userTabs) do
-		num = tonumber( string.sub(name, 4) ) 
-		table.insert(numOrder, num)
+	for name,value in pairs( userTabs ) do
+		num = tonumber( string.sub(name, 4) );
+		table.insert( numOrder, num );
 	end
 
 	table.sort(numOrder)
 
-	for i, order in ipairs(numOrder) do
-		for name,value in pairs(userTabs) do
-			if tonumber( string.sub(name, 4) ) == order then
-				tab = createControl( "tabbertab", name )
-
-				if name ~= "TAB1" then 
-					tab.canDelete() 
-				else 
-					mainTab = tab 
-				end
-
-				tab.setValue(userTabs[name]["text"])
+	for i, order in ipairs( numOrder ) do
+		for name,value in pairs( userTabs ) do
+			if tonumber( string.sub( name, 4 ) ) == order then
+				tab = createControl( "tabbertab", name );
+				tab.load( value );
 			end
 		end
 	end
 
-	currentTab = mainTab
-	resetAnchors()
-	devMessage( "Tabs Loaded" )
+	resetAnchors();
 end
 
-function storeTab( tab )
-	devMessage( "Storing Tab: " .. tab.getName() )
+-- Add a new tab
 
-	userTabs[tab.getName()] = {}
-	userTabs[tab.getName()]["init"] = {}
-	userTabs[tab.getName()]["data"] = {}
-	userTabs[tab.getName()]["text"] = tab.getValue()
+function addTab()
+	tabs = getControls();
+	num = tonumber( string.sub(tabs[#tabs].getName(), 4) ) + 1;
+
+	newTab = createControl( "tabbertab", "TAB" .. num );
+	newTab.new( "New Tab " .. num );
+
+	resetAnchors();
 end
 
-function changeTab( tab )
-	devMessage( "Changing To Tab: " .. tab.getName() )
-
-	saveAllWindows()
-	switchTab = true
-	closeAllWindows()
-	loadAllWindows( tab )
-	switchTab = false
-	tabGainFocus( tab )
-	devMessage( "Tab Changed" )
-end
-
-function tabGainFocus( tab )
-	tabLoseFocus( currentTab )
-	currentTab = tab
-	currentTab.setBackColor("#FF222222")
-	currentTab.setColor("#FFDDDDDD")
-end
-
-function tabLoseFocus( tab )
-	currentTab.setBackColor("#FF333333")
-	currentTab.setColor("#FFCCCCCC")
-end
+-- Delete passed tab
 
 function deleteTab( tab )
-	devMessage( "Deleting tab: " .. tab.getName() )
-
-	if tab == currentTab then
-		changeTab( mainTab )
-	end
-	userTabs[tab.getName()] = nil
+	tab.closeAllWindows()
 	tab.destroy()
+	userTabs[tab.getName()] = nil
+
+	if currentTab == tab then
+		currentTab = nil
+	end
+
 	resetAnchors()
-	devMessage( "Tab Deleted" )
 end
+
+-- Switch tabs
+-- TODO: if switched tab = current tab, return to the tempTab
+
+function switchTab( tab )
+	if currentTab then
+		currentTab.loseFocus();
+	end
+	tab.gainFocus();
+	currentTab = tab;
+end
+
+-- Resets anchors of all child tabs to stack them left
 
 function resetAnchors()
 	controls = getControls()
 
-	for i,tab in ipairs(controls) do
-		if i > 1 then
-			tab.setAnchor("left", controls[i-1].getName(), "right", "absolute", 30)
-		end
-	end
-end
-
-function captureData( data )
-	userTabs[currentTab.getName()]["init"][recentWindow] = data
-	Debug.console(userTabs[currentTab.getName()]["init"])
-
-	return data
-end
-
-function saveWindow( window )
-	if not switchTab then
-		devMessage( "Saving Window: " .. window.getClass() )
-
-		local class = window.getClass()
-		local node = window.getDatabaseNode()
-		local nodePath = ""
-		local x,y = window.getPosition()
-		local w,h = window.getSize()
-
-		if node ~= nil then
-			nodePath = node.getPath()
-		end
-
-		if nodePath == "" then
-			userTabs[currentTab.getName()]["data"][class] = {
-				["class"] = class,
-				["node"] = nodePath,
-				["position"] = {
-					["x"] = x,
-					["y"] = y
-				},
-				["size"] = {
-					["width"] = w,
-					["height"] = h
-				}
-			}
-			recentWindow = class
+	for i, tab in ipairs(controls) do
+		if i = 1 then
+			tab.setAnchor("left", getName(), "left", "absolute", 20);
 		else
-			userTabs[currentTab.getName()]["data"][nodePath] = {
-				["class"] = class,
-				["node"] = nodePath,
-				["position"] = {
-					["x"] = x,
-					["y"] = y
-				},
-				["size"] = {
-					["width"] = w,
-					["height"] = h
-				}
-			}
-			recentWindow = nodePath
+			tab.setAnchor("left", controls[i-1].getName(), "right", "absolute", 30);
 		end
 	end
 end
 
-function removeWindow( window )
-	if not switchTab and not dClose then
-		devMessage( "Removing Window: " .. window.getClass() )
+-- If there is a tab open, and desktop closes, save the tab
 
-		if window.getDatabaseNode() == nil then
-			userTabs[currentTab.getName()]["data"][window.getClass()] = nil
-			userTabs[currentTab.getName()]["init"][window.getClass()] = nil
-		else
-			userTabs[currentTab.getName()]["data"][window.getDatabaseNode().getPath()] = nil
-			userTabs[currentTab.getName()]["init"][window.getDatabaseNode().getPath()] = nil
-		end
+function onDesktopClose()
+	if currentTab then
+		currentTab.saveAllWindows()
 	end
 end
 
-function saveAllWindows( )
-	devMessage( "Saving all windows" )
+-- When a window opens, and we have a tab selected, save the tab
 
-	for name, data in pairs( userTabs[currentTab.getName()]["data"] ) do
-		window = Interface.findWindow( data["class"], data["node"] )
-		if window ~= nil then
-			saveWindow( window )
-		else
-			userTabs[currentTab.getName()]["data"][name] = nil
-		end 
+function onWindowOpened( window )
+	if currentTab then
+		currentTab.storeWindow( window )
 	end
 end
 
-function closeAllWindows()
-	devMessage( "Closing all windows" )
+-- When a window closes, and we have a tab selected, remove the tab
 
-	for name, window in pairs( userTabs[currentTab.getName()]["data"] ) do
-		devMessage ( "Closing window: " .. name )
-		Interface.findWindow( window["class"], window["node"] ).close()
+function onWindowClosed( window )
+	if currentTab then
+		currentTab.removeWindow( window )
 	end
-end
 
-function loadAllWindows( tab )
-	devMessage( "Loading all windows" )
-
-	for name,window in pairs( userTabs[tab.getName()]["data"] ) do
-		devMessage( "Loading window: " ..name)
-
-		instance = Interface.openWindow(window["class"], window["node"])
-		instance.setPosition( window["position"]["x"], window["position"]["y"], true )
-		instance.setSize( window["size"]["width"], window["size"]["height"] )
-
-		if userTabs[tab.getName()]["init"][name] ~= nil then
-			Debug.console( userTabs[tab.getName()]["init"][name] )
-			instance.init( userTabs[tab.getName()]["init"][name] )
-		end
-
-	end
-end
-
-function desktopClose()
-	devMessage( "Desktop Closing" )
-
-	saveAllWindows()
-	dClose = true
 end
--- lua/TabberTab.lua
+++ lua/TabberTab.lua
@@ -1,50 +1,154 @@
-canDelete = false
+tabData 		= nil;
+recentWindow 	= nil;
+closing 		= false;
+opening 		= false;
 
 function onInit()
-	registerMenuItem("Rename Tab", "edit", 5)
+	registerMenuItem("Rename Tab", "edit", 5);
+	registerMenuItem("Delete Tab", "delete", 4);
 end
 
 function onMenuSelection( selection )
 	if selection == 5 then
-		setReadOnly( false )
-		setFocus( true )
+		setReadOnly( false );
+		setFocus( true );
 	end
 	if selection == 4 then
-		Interface.findWindow("Tabber", "").deleteTab( self )
+		Interface.findWindow("Tabber", "").deleteTab( self );
 	end
 end
 
 function onEnter()
-	setReadOnly( true )
-	checkEmpty()
-	saveValue()
+	setReadOnly( true );
+	checkEmpty();
+	saveValue();
 end
 
 function onLoseFocus()
-	setReadOnly( true )
-	checkEmpty()
-	saveValue()
+	setReadOnly( true );
+	checkEmpty();
+	saveValue();
 end
 
 function onClickDown( button, x, y )
 	if button == 1 then
-		Interface.findWindow("Tabber", "").changeTab( self )
+		Interface.findWindow("Tabber", "").changeTab( self );
 	end
 end
 
 -- Custom functions
 
+function load( data )
+	tabData = data;
+	setValue( tabData["text"] )
+end
+
+function new( text )
+	tabData = {};
+	tabData["init"] = {};
+	tabData["data"] = {};
+	tabData["text"] = text;
+	setValue( text );
+end
+
 function checkEmpty()
 	if getValue() == "" then
-		setValue( "Unnamed" )
+		setValue( "Unnamed" );
 	end
 end
 
 function saveValue()
-	thisNode = Interface.findWindow("Tabber", "").storeTab( self )
+	tabData["text"] = getValue();
 end
 
-function canDelete()
-	canDelete = true
-	registerMenuItem("Delete Tab", "delete", 4)
+function gainFocus()
+	loadAllWindows();
+	etBackColor("#FF222222");
+	setColor("#FFDDDDDD");
+end
+
+function loseFocus()
+	saveAllWindows();
+	closeAllWindows();
+	setBackColor("#FF333333");
+	setColor("#FFCCCCCC");
+end
+
+function storeWindow( window )
+	if not opening then
+		local class = window.getClass();
+		local node = window.getDatabaseNode().getPath();
+		local x,y = window.getPosition();
+		local w,h = window.getSize();
+
+		if node == nil then
+			node = "";
+		end
+
+		local windowName = class .. node;
+		recentWindow = windowName;
+
+		tabData["data"][windowName] = {
+			["class"] = class,
+			["node"] = node,
+			["position"] = {
+				["x"] = x,
+				["y"] = y
+			},
+			["size"] = {
+				["width"] = w,
+				["height"] = h
+			}
+		};
+	end
+end
+
+function removeWindow( window )
+	if not closing then
+		local class = window.getClass();
+		local node = window.getDatabaseNode.getPath();
+		local windowName = class .. node;
+
+		tabData["data"][windowName] = nil;
+		tabData["init"][windowName] = nil;
+	end
+end
+
+function saveAllWindows()
+	for windowName, data in pairs( tabData["data"] ) do
+		window = Interface.findWindow( data["class"], data["node"] );
+		storeWindow( window );
+	end
+end
+
+function closeAllWindows()
+	closing = true;
+
+	for windowName, data in pairs( tabData["data"] ) do
+		Interface.findWindow( data["class"], data["node"] ).close();
+	end
+
+	closing = false;
+end
+
+function loadAllWindows()
+	opening = true;
+
+	for windowName, data in pairs( tabData["data"] ) do
+		window = Interface.openWindow( data["class"], data["node"] );
+		window.setPosition( data["position"]["x"], data["position"]["y"], true );
+		window.setSize( data["size"]["width"], data["size"]["height"] );
+
+		if tabData["init"][windowName] then
+			window.init( userTabs[tab.getName()]["init"][name] );
+		end
+
+	end
+
+	opening = false;
+end
+
+function captureData( data )
+	tabData["init"][recentWindow] = data;
+	return data;
 end
